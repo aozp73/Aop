83bdf3e2fb9e729d2a515c5f4266e999606339d6
aozp73 <aozp73@naver.com>
2023-03-17 17:20:38 +0900

    AOP 기초 코드 구현 완료

---
+++ src/main/java/shop/mtcoding/aop/handler/LoginInterceptor.java
@@ -0,0 +1,27 @@
+package shop.mtcoding.aop.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import shop.mtcoding.aop.model.User;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        HttpSession session = request.getSession();
+        User principal = (User) session.getAttribute("principal");
+
+        if (principal == null) {
+            response.setContentType("text/html; charset=utf-8");
+            response.getWriter().println("잘못된 접근입니다.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
